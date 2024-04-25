e75ac20829666054478b6008861b5bf6c8ab4030
Quentin Dispas <qdispas@gmail.com>
2024-04-25 16:09:22 +0200

    start of the project

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+// Exporte une fonction qui configure Eleventy
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/assets/");
+  eleventyConfig.addPassthroughCopy("src/css/");
+  eleventyConfig.addWatchTarget("src/css/");
+  // Retourne un objet contenant les options de configuration pour Eleventy
+  return {
+    // Répertoires d'entrée, d'inclusions et de sortie
+    dir: {
+      input: "src", // Répertoire contenant les fichiers source
+      includes: "_includes", // Répertoire contenant les fragments de code réutilisables
+      data: "_data",
+      output: "dist", // Répertoire où seront générés les fichiers HTML
+    },
+    // Formats de fichier de modèle pris en charge
+    templateFormats: ["md", "njk", "html"],
+    // Moteur de modèle Markdown
+    markdownTemplateEngine: "njk",
+    // Moteur de modèle HTML
+    htmlTemplateEngine: "njk",
+    // Moteur de modèle pour les fichiers de données
+    dataTemplateEngine: "njk",
+  };
+};
