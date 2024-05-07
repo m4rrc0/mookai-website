7cf4fb0951d246ba89c0f0b271ada2a49ac13028
Quentin Dispas <qdispas@gmail.com>
2024-05-07 14:43:07 +0200

    Creation de la navabar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        menulg: "100px 1fr 50px",
+        menuxl: "100px 1fr 100px"
+      },
       colors: {
         deums: "#D9933D",
+        arriereplan: "#F7F7F7"
       },
       fontFamily: {
         sans: ["Roboto Condensed", "ui-sans-serif", "system-ui", "sans-serif"],
