39de277dee5f5a22b716b2bee7a773470a275653
Quentin Dispas <qdispas@gmail.com>
2024-05-15 22:49:17 +0200

    corrections svg (footer + navabar) + sections témoignages et contact

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js,njk}"],
   theme: {
     extend: {
 
@@ -8,9 +8,9 @@ module.exports = {
         'hero-pattern': "url(/assets/images/reunion.jpg)",
       },
       gridTemplateColumns: {
-        menulg: "100px 1fr 50px",
-        menuxl: "100px 1fr 100px",
-        temoignage : "100px 1fr 1fr",
+        menulg: "9rem 1fr 50px",
+        menuxl: "9rem 1fr 100px",
+        temoignage : "9rem 1fr 1fr",
       },
       colors: {
         deums: "#D9933D",
@@ -19,7 +19,7 @@ module.exports = {
         orange: "#D9933D",
         sanguine: "#D94423",
         brun: "#A2724E",
-        temoin: "#00000029",
+        temoin: "#CFCFCF",
       },
       fontSize: {
         nav: "24px",
