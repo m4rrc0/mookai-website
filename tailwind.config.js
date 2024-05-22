a6e8bf60e72c5683f1d3147d8e2eefed549ec4b8
Quentin Dispas <qdispas@gmail.com>
2024-05-22 21:57:45 +0200

    section valeur big screen (not responsive yet)

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,10 @@ module.exports = {
         menuxl: "9rem 1fr 100px",
         temoignage : "9rem 1fr 1fr",
         apropos : "9rem 1fr",
+        valeurs : "9rem 416px 316px 404px",
+      },
+      gridTemplateRows: {
+        'vals': 'repeat(12, 72px)',
       },
       colors: {
         deums: "#D9933D",
@@ -26,6 +30,8 @@ module.exports = {
         brunclair: "#978076",
         temoin: "#CFCFCF",
         blob: "#8d8298",
+        beige: "#ECD5C3",
+        bordeau: "#731e16",
       },
       fontSize: {
         nav: "24px",
