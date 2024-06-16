096937a7e1e9267f0280fedca1fa1762e40e2522
m4rrc0 <marcoet@gmail.com>
2024-06-16 23:15:56 +0200

    feat: styleguide noindex

--- src/assets/js/toggleMenu.js
+++
@@ -1,6 +0,0 @@
-const menuElementsToToggle = document.querySelectorAll(".toggle-menu");
-const iconeToggle = document.querySelector(".icone-toggle");
-
-const toggleMenu = () => menuElementsToToggle.forEach(el => el.classList.toggle("hidden"));
-
-iconeToggle.addEventListener("click", toggleMenu);
